349501f383a4884956c524a026d620a015a536a1
Zhijian Li <zhijian.li@rwth-aachen.de>
2022-06-08 15:13:58 +0200

    update tutorial

--- run.sh
+++ run.sh
@@ -15,4 +15,5 @@ source ~/.bashrc
 conda activate r-4.1
 
 R -e "devtools::document()"
-R -e "pkgdown::build_site(preview = FALSE, lazy=TRUE)"
+#R -e "pkgdown::build_site(preview = FALSE, lazy=TRUE)"
+R -e "pkgdown::build_site(preview = FALSE)"
