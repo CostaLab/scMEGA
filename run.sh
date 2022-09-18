900e92fcd0cfffb1fafeb7c092a08832ac20f331
Zhijian Li <zhijian.li@rwth-aachen.de>
2022-09-18 18:27:28 +0200

    update run.sh

--- run.sh
+++ run.sh
@@ -15,5 +15,5 @@ source ~/.bashrc
 conda activate r-4.1
 
 R -e "devtools::document()"
-#R -e "pkgdown::build_site(preview = FALSE, lazy=TRUE)"
-R -e "pkgdown::build_site(preview = FALSE, lazy=FALSE)"
+R -e "pkgdown::build_site(preview = FALSE, lazy=TRUE)"
+#R -e "pkgdown::build_site(preview = FALSE, lazy=FALSE)"
